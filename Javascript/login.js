656c9ba366c4f7f40ee0c0951fb0ecdaa3a9f085
Azat-Akash <72602899+Azat-Akash@users.noreply.github.com>
2023-11-07 20:08:04 +0600

    login page initial commit

---
+++ Javascript/login.js
@@ -0,0 +1,21 @@
+const server =
+    "https://4f26172a-cec4-4fd9-adc2-28a21e50172f.mock.pstmn.io/login";
+
+const signInBtn = document.querySelector("#sign-in");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+signInBtn.onclick = async function () {
+    const admin = {
+        email: email.value,
+        password: password.value,
+    };
+    const body = JSON.stringify(admin);
+
+    const response = await fetch(server, {
+        body: body,
+        method: "POST",
+    });
+    const responseBody = await response.text();
+    console.log(responseBody);
+};
